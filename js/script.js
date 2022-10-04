7fa68fb842b246f692535ce0b08b00ada2a5f40e
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-10-04 14:19:41 -0400

    js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Julienka Sohal All rights reserved
+//
+// Created by: Julienka Sohal
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+
+function myButtonClicked () {
+  document.getElementById ("Hello-World").innerHTML = "<p>Hello World!</p>"
+}
